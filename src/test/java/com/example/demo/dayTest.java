5bc0df91fc7de5bfefe8247323f7df1cb24cb446
cchu <hcc_sf@jztey.com>
2021-08-27 11:45:24 +0800

    HelloGogs

---
+++ src/test/java/com/example/demo/dayTest.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author : cchu
+ * Date: 2021/8/25 17:01
+ */
+@SpringBootTest
+public class dayTest {
+    @Test
+    void contextLoads() {
+        System.out.println("HelloWorld！！！");
+        System.out.println("HelloGitHub");
+        System.out.println("HelloGogs");
+
+    }
+}
